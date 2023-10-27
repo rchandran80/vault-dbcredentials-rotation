981ffe4a3ebea2cdf3aaa2856ce37c32efe03d7d
rchandran80 <roops4fun@gmail.com>
2023-10-27 13:45:39 -0400

    Updates made to rotate, implemented revoke,handled conditions for other events with error handling.

--- src/main/java/com/example/springbootvaultdemo/SpringBootVaultDemoApplication.java
+++ src/main/java/com/example/springbootvaultdemo/SpringBootVaultDemoApplication.java
@@ -2,31 +2,14 @@ package com.example.springbootvaultdemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication
 @Component
 public class SpringBootVaultDemoApplication {
-
-@Autowired
-private VaultConfiguration config1 = null;
-
-	private static final Logger logger = LoggerFactory.getLogger(SpringBootVaultDemoApplication.class.getName());
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootVaultDemoApplication.class, args);
-	}
-
-	@PostConstruct
-	public void postConstruct() {
-		logger.info("Reading properties from autowired config object inside SpringBootVaultDemo");
-		logger.info("Min Renewal = "+config1.minRenewal);
-		logger.info("Exp Threshold = "+config1.expiryThreshold);
-		logger.info("isEnabled = "+config1.isEnabled);
 	}
 }
